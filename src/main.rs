d672c586f692414640e31ce08f4d375651d355fd
kuznetsovin <kuznetsovin@gmail.com>
2017-08-15 11:02:20 +0300

    рассылка разнесена по отдельным потокам

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ mod inoreader;
 mod telegram;
 mod store;
 
-//use std::thread;
+use std::thread;
+use std::process;
 use std::time::Duration;
 use std::thread::sleep;
 
@@ -16,6 +17,10 @@ fn main() {
     // получаем список аргументов командной строки
     let cmd_args: Vec<_> = std::env::args().collect();
 
+    if cmd_args.len() < 2 {
+        println!("Illegal parameter use: inobot <config_path>");
+        process::exit(1);
+    }
     let cfg = config::Config::new(&cmd_args[1]);
     let store = store::Store::connect(&cfg);
 
@@ -36,10 +41,13 @@ fn main() {
         let news: Vec<inoreader::News> = inoreader_client.get_last_news(unread_count);
 
         for c in saved_charts {
-            for n in news.clone() {
-                telegram_bot.send_message(n, c);
-            }
-            //            thread::spawn(move || {}).join();
+            let sending_news = news.clone();
+            let mut bot = telegram::TelegramBotClient::new(&cfg);
+            let _ = thread::spawn(move || {
+                for n in sending_news {
+                    bot.send_message(n, c);
+                }
+            }).join();
         }
 
         // ждем timeout в секундах для следующего обращения
